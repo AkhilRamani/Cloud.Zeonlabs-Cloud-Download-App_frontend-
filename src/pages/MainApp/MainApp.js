7ed1671834b8f920eac1ba8c5b65d0a108b4cc1f
Akhil <ramaniakhil094@gmail.com>
2019-11-27 15:09:00 +0530

    socket.on -> connect listener removed

--- src/pages/MainApp/MainApp.js
+++ src/pages/MainApp/MainApp.js
@@ -1,15 +1,15 @@
 import React from "react"
 
 import ListContainer from '../../components/navigations/ListContainer/ListContainer'
-import {socket} from '../../apis/socket/socket'
+// import {socket} from '../../apis/socket/socket'
 import { HeaderSidebarWrapper } from "../../components/navigations/HeaderSidebarWrapper"
 
 
 class MainApp extends React.Component {
 
-    componentDidMount() {
-        socket.on('connect', () => console.log('socket server connected'))
-    }
+    // componentDidMount() {
+        // socket.on('connect', () => console.log('socket server connected'))
+    // }
 
     render(){
         return ( 
