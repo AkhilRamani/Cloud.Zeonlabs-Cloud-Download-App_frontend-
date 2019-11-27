d0e48f8c02582d02774ee72f7ec8fefd64c93956
Akhil <ramaniakhil094@gmail.com>
2019-11-27 15:10:20 +0530

    socket file listeners logs removed

--- src/apis/socket/socket.listener.js
+++ src/apis/socket/socket.listener.js
@@ -8,13 +8,13 @@ import {notifyMsgs, fileStatus} from '../../common/constants'
 export const socketFileStatusListener = (userId) =>{
 
     socket.on(`${socketEvents.DONE}-${userId}`, ({_id}) => {
-        console.log('file uploaded ', _id)
+        // console.log('file uploaded ', _id)
         store.dispatch(changeFileStatus(_id, fileStatus.DONE))
         notify(notifyMsgs.DOWNLOADED_MSG)
     })
 
     socket.on(`${socketEvents.FAILED}-${userId}`, ({_id}) => {
-        console.log('file upload failed', _id)
+        // console.log('file upload failed', _id)
         store.dispatch(changeFileStatus(_id, fileStatus.FAILED))
         notify(notifyMsgs.DOWNLD_FAILED)
     })
